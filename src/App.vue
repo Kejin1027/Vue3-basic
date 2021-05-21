6346b9e5b59d810070e6de0b1cb5d541212ec429
Kejin <kdon0003@student.monash.edu>
2021-05-21 17:27:32 +0800

    basics

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,40 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <h1>{{count}}</h1>
+  <h1>{{double}}</h1>
+  <ul>
+    <li v-for="num in number" :key="num">{{num}}</li>
+  </ul>
+  <h1>{{person.name}}</h1>
+  <button @click="increase">INCREASE</button>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent, reactive, computed, toRefs } from 'vue';
+interface DataProps {
+  count: number;
+  double: number;
+  increase: () => void;
+  number: number[];
+  person: { name?: string};
+}
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
+  setup() {
+    const data: DataProps = reactive({
+      count: 0,
+      increase: () => { data.count++ },
+      double: computed(() => data.count * 2),
+      number: [0, 1, 2, 3],
+      person: {}
+    })
+    data.number[0] = 5
+    data.person.name = "kkk"
+    const refData = toRefs(data)
+    return{
+      ...refData
+    }
   }
 });
 </script>
